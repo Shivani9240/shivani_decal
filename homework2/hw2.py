dc4ce8088e0f86f5258db4044760f21a9d49cb67
Shivani <shivani_kulkarni@berkeley.edu>
2025-02-11 17:09:38 -0800

    Adding hw2 folder

---
+++ homework2/hw2.py
@@ -0,0 +1,20 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    #The cd ~/Desktop command would allow you to move directly to the Desktop
+# 2) What does ~/ mean?
+    # The ~/ means the home directory. It is a faster way to refererence the home directory without typing the whole thing.
+# 3) What's the difference between and abolute and a relative path in your terminal?
+    #An absolute path in the terminal refers to to the full path starting at the root and making the way up or down. It is not dependent on the current directory that the user is in. Relative path, however, is dependent on the current directory that one is in and it refers to a path relative to this current location (cd ..)
+# 4) What command would bring you back to your home directory?
+    #The command cd by itself returns us back to the home directory
+# 5) If you called rm ./ in your current working directory, what would it do? 
+    #It wouldn't do anything because there is nothing we prompted it to remove. The ./ refers to the current directory but since rm along can only remove files, this command won't do anything.
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    #The git add command allows files to be included in teh next committ - it pushes things from the current working directory to the staging area. The git committ command takes things one step further and records the staged changes in the local repository. Lastly git push will push the changes from the local repository to the remote repository (GitHub)
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    # Git add is a command that will add whatever files/directories you want into the staging area ready for committ, and git status will show the user the status of what is in the staging area and ready to committ.
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    # This is not exactly an error/bug but the most frustrating challenge I've encounetred in this class is having to download VS code like 3 times since for some reason it wasn't working properly. One bug that was troublesome is having two different names for shivani_decal because I also had shivani_decal2 but then I renamed the file so everything got fixed.
+# 9) How does cloning a repository differ from pulling from a repository?
+    # Cloning a repository will copy the entire repository into your computer (like we cloned brianna_repo) and pulling from a repository will update any changes that were made from the repository.
+# 10) Tell me a fun fact!
+    #Four Galilean Moons: Callisto, Ganymede, Io, and Europa
